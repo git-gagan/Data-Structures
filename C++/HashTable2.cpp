fad583c3b20cec2f037214def151fa8776816a6f
Bunny <59775180+git-gagan@users.noreply.github.com>
2020-10-31 14:53:37 +0530

    Update HashTable2.cpp

--- C++/HashTable2.cpp
+++ C++/HashTable2.cpp
@@ -1,3 +1,10 @@
+/*
+Our friend Monk has been made teacher for the day today by his school professors .
+He is going to teach informatics to his colleagues as that is his favorite subject . Before entering the class, Monk realized that he does not remember the names of all his colleagues clearly . He thinks this will cause problems and will not allow him to teach the class well. However, Monk remembers the roll number of all his colleagues very well . Monk now wants you to help him out. He will initially give you a list indicating the name and roll number of all his colleagues.
+When he enters the class he will give you the roll number of any of his colleagues belonging to the class.
+You need to revert to him with the name of that colleague.
+*/
+
 #include<iostream>
 #include<string>
 using namespace std;
